1aa4236fab00d630a76dcfe0532efd98149538e0
rukiyetunc <tuncc.rukiye@gmail.com>
2023-09-19 22:17:45 +0300

    .

---
+++ my-app/src/Components/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default function Header() {
+  return (
+    <div>
+        <img src="./images/Troll Face.png"></img>
+        <h1 className="header">Mame Generator</h1>
+        <h5 className="">React Coddddddddddddddddurse - Project 3</h5>
+    </div>
+  );
+}
