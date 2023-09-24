c2327ccb17e7dc0d89ded45579d39953c7ed6669
rukiyetunc <tuncc.rukiye@gmail.com>
2023-09-24 21:25:09 +0300

    .

--- my-app/src/Components/Meme.js
+++ my-app/src/Components/Meme.js
@@ -1,29 +1,67 @@
-import React from "react";
+// import React from "react";
+// import memeData from "../memeData";
+
+// export default function Meme() {
+//   const [memeImage, setMemeImege] = React.useState("");
+
+//   function getMemeImage() {
+//     const memeArray = memeData.data.memes;
+//     const randomNumber = Math.floor(Math.random() * memeArray.length);
+//     setMemeImege(memeArray[randomNumber].url);
+//   }
+
+//   return (
+//     <main>
+//       <div>
+//         <form className="form">
+//           <input type="text" placeholder="Top text" className="form-input" />
+//           <input type="text" placeholder="Bottom text" className="form-input" />
+//           <button className="form-button" onClick={getMemeImage}>
+//             Get a new meme image 🖼
+//           </button>
+//         </form>
+//       </div>
+//         <img src={memeImage} className="meme-image" />
+//     </main>
+//   );
+// }
+import React, { useState, useEffect } from "react";
 import memeData from "../memeData";
 
 export default function Meme() {
-  const [memeImage, setMemeImege] = React.useState("");
+  const [memeImage, setMemeImage] = useState("");
+  const [key, setKey] = useState(Date.now()); // Rastgele bir key oluştur
 
   function getMemeImage() {
     const memeArray = memeData.data.memes;
     const randomNumber = Math.floor(Math.random() * memeArray.length);
-    // // setMemeImege(memeArray[randomNumber].url)
-    const url = memeArray[randomNumber].url;
-    console.log(url);
+    const newMemeImage = memeArray[randomNumber].url;
+
+    setMemeImage(newMemeImage);
+    setKey(Date.now()); // Key'i güncelle
   }
 
+  useEffect(() => {
+    getMemeImage(); // Sayfa yüklendiğinde ilk resmi al
+  }, []);
+
   return (
-    <div>
-      <main>
+    <main>
+      <div>
         <form className="form">
           <input type="text" placeholder="Top text" className="form-input" />
           <input type="text" placeholder="Bottom text" className="form-input" />
           <button className="form-button" onClick={getMemeImage}>
             Get a new meme image 🖼
           </button>
-          {/* <img src={memeImage} className="meme-image" /> */}
         </form>
-      </main>
-    </div>
+      </div>
+      <img
+        key={key} // Resmi güncellemek için key ekledik
+        src={memeImage}
+        className="meme-image"
+        alt="Meme"
+      />
+    </main>
   );
 }
--- my-app/src/memeData.js
+++ my-app/src/memeData.js
@@ -15,25 +15,19 @@ export default{
                 "id":"11",
                 "name": "Two buttons",
                 "url":"https://img.paratic.com/dosya/2015/01/tum-zamanlarin-en-pahali-animasyon-filmleri.jpg",
-                "width":600,
-                "height": 908,
+                "width": 1200,
+                "height": 1200,
                 "box_count": 3
             },
             {
                 "id":"12",
-                "name": "Two buttons",
+                "name": "Three buttons",
                 "url":"https://frpnet.net/wp-content/uploads/2012/05/yaz-filmi-brave.jpg",
-                "width":700,
-                "height": 988,
+                "width": 1200,
+                "height": 1200,
                 "box_count": 4
-            },  {
-                "id":"13",
-                "name": "Two buttons",
-                "url":"https://frpnet.net/wp-content/uploads/2012/05/yaz-filmi-brave.jpg",
-                "width":700,
-                "height": 988,
-                "box_count": 4
-            },
+            },  
+               
         ]
     }
 }
